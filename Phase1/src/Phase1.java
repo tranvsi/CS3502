4689d62c53ff1b94632a1b05f271893033fc4776
John Tran <johntr2002@gmail.com>
2024-09-18 20:40:21 -0400

    Developed basic structure for program

--- Phase1/src/Phase1.java
+++ Phase1/src/Phase1.java
@@ -1,2 +1,17 @@
 public class Phase1 {
+    public static void main(String[] args){
+
+    }
+
+    static class ReaderThread implements Runnable{
+
+    }
+
+    static class WriterThread implements Runnable{
+
+    }
+
+    static class CalculateThread implements Runnable{
+
+    }
 }
