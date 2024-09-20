1540be4083aa29042bd028b76f9d568ac62a3fa7
John Tran <johntr2002@gmail.com>
2024-09-20 10:31:52 -0400

    created the initial methods and getters for the bank account class

--- Phase3/src/Phase3.java
+++ Phase3/src/Phase3.java
@@ -5,4 +5,34 @@ public class Phase3 {
     public static void main(String[] args){
 
     }
+
+    static class BankAccount {
+        private double balance;
+        private final ReentrantLock lock = new ReentrantLock();
+
+        public BankAccount(double initialBalance){
+            this.balance = initialBalance;
+        }
+
+        public ReentrantLock getLock(){
+            return lock;
+        }
+
+        public double getBalance(){
+            return balance;
+        }
+
+        public void deposit(double amount){
+            balance += amount;
+        }
+
+        public void withdraw(double amount){
+            balance -= amount;
+        }
+
+        @Override
+        public String toString(){
+            return "Account@" + hashCode();
+        }
+    }
 }
