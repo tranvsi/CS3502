407fb610ef3c098b74b0789919fe9486ee2da52d
John Tran <johntr2002@gmail.com>
2024-09-20 13:41:40 -0400

    setup bank account class and transfer method

--- Phase4/src/Phase4.java
+++ Phase4/src/Phase4.java
@@ -2,4 +2,30 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.TimeUnit;
 
 public class Phase4 {
+    public static void main(String[] args){
+
+    }
+
+    public static void transfer(){
+
+    }
+
+    static class BankAccount implements Comparable<BankAccount>{
+        private double balance;
+        private final ReentrantLock lock = new ReentrantLock();
+
+        public BankAccount(double startingBalance){
+            this.balance = startingBalance;
+        }
+
+        public ReentrantLock getLock(){
+            return lock;
+        }
+
+        public double getBalance(){
+            return balance;
+        }
+
+
+    }
 }
