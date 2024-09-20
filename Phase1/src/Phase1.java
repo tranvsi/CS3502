5c671978f2c873480b72661f42821cda07d78e7f
John Tran <johntr2002@gmail.com>
2024-09-19 21:15:48 -0400

    Created two synchronized methods in bank account class

--- Phase1/src/Phase1.java
+++ Phase1/src/Phase1.java
@@ -84,5 +84,27 @@ public class Phase1 {
 
     static class BankAccount{
         private double balance;
+
+        public synchronized void deposit(double money){
+            double newBalance = balance + money;
+
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e){
+                System.out.println("Deposit Error!");
+            }
+            balance = newBalance;
+        }
+
+        public synchronized void withdraw(double money){
+            double newBalance = balance - money;
+
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e){
+                System.out.println("Withdraw error");
+            }
+            balance = newBalance;
+        }
     }
 }
