9dae338c779b574d4015618a20e3f5376c23c858
John Tran <johntr2002@gmail.com>
2024-09-19 21:47:06 -0400

    Imported code from phase1 without comments

--- Phase2/src/Phase2.java
+++ Phase2/src/Phase2.java
@@ -1,5 +1,61 @@
 public class Phase2 {
     public static void main(String[] args){
 
+        BankAccount bank = new BankAccount();
+
+        Thread deposit = new Thread(() -> {
+            bank.deposit(100.0);
+            System.out.println("$100 has been deposited.");
+        }, "DepositThread");
+
+        Thread withdraw = new Thread(() -> {
+            bank.withdraw(75.0);
+            System.out.println("$75 has been withdrawn.");
+        }, "WithdrawThread");
+
+        deposit.start();
+        withdraw.start();
+
+        try{
+            deposit.join();
+            withdraw.join();
+        } catch (InterruptedException e){
+            System.err.println("Main thread error");
+        }
+        //Display final account balance
+        System.out.println("Account balance: $" + bank.returnBalance());
+        //Close program
+        System.exit(0);
+    }
+
+    static class BankAccount{
+
+        private double balance;
+
+        public synchronized void deposit(double money){
+            double newBalance = balance + money;
+
+            try{
+                Thread.sleep(500);
+            } catch (InterruptedException e){
+                System.out.println("Deposit Error!");
+            }
+            balance = newBalance;
+        }
+
+        public synchronized void withdraw(double money){
+            double newBalance = balance - money;
+
+            try{
+                Thread.sleep(500);
+            } catch (InterruptedException e){
+                System.out.println("Withdraw error");
+            }
+            balance = newBalance;
+        }
+
+        public synchronized double returnBalance(){
+            return balance;
+        }
     }
 }
