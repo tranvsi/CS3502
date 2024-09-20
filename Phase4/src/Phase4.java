7c18290eafee9d516f977fa8d3faaed58be68a31
John Tran <johntr2002@gmail.com>
2024-09-20 14:04:34 -0400

    created try-catch block in main method to join threads before outputting final balances

--- Phase4/src/Phase4.java
+++ Phase4/src/Phase4.java
@@ -6,11 +6,20 @@ public class Phase4 {
         BankAccount bank1 = new BankAccount(5000);
         BankAccount bank2 = new BankAccount(5000);
 
-        Thread thread1 = new Thread(() -> transfer(bank1, bank2, 250), "Transfer1to2");
-        Thread thread2 = new Thread(() -> transfer(bank2, bank1, 250), "Transfer2to1");
+        Thread thread1 = new Thread(() -> transfer(bank1, bank2, 550), "Transfer1to2");
+        Thread thread2 = new Thread(() -> transfer(bank2, bank1, 1500), "Transfer2to1");
 
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("Bank 1 balance: " + bank1.getBalance());
+        System.out.println("Bank 2 balance: " + bank2.getBalance());
     }
 
     public static void transfer(BankAccount from, BankAccount to, double amount){
@@ -18,16 +27,16 @@ public class Phase4 {
         BankAccount secondAcc = from.compareTo(to) < 0 ? to:from;
 
         try {
-            if (firstAcc.getLock().tryLock(1, TimeUnit.SECONDS)) {
-                System.out.println(Thread.currentThread().getName() + " lock " + firstAcc);
+            if (firstAcc.getLock().tryLock(5, TimeUnit.SECONDS)) {
+                System.out.println(Thread.currentThread().getName() + " locked " + firstAcc);
 
                 try {
-                    if (secondAcc.getLock().tryLock(1, TimeUnit.SECONDS)) {
+                    if (secondAcc.getLock().tryLock(5, TimeUnit.SECONDS)) {
                         System.out.println(Thread.currentThread().getName() + " locked " + secondAcc);
                         if (from.getBalance() >= amount){
                             from.withdraw(amount);
                             to.deposit(amount);
-                            System.out.println("Moved $" + amount " from " + from + " to " + to);
+                            System.out.println("Moved $" + amount + " from " + from + " to " + to);
                         }
                         else {
                             System.out.println("Insufficient balance to transfer from " + from + " to " + to);
